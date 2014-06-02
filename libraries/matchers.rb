e2e553ed2aacf2bb28c0bac04f10177aca6af1e0
Aaron Lane <aaron_lane@otpp.com>
2014-06-02 13:38:02 -0400

    Add ChefSpec matchers

---
+++ libraries/matchers.rb
@@ -0,0 +1,13 @@
+if defined?(ChefSpec)
+  def install_tar_package(source)
+    ChefSpec::Matchers::ResourceMatcher.new(:tar_package, :install, source)
+  end
+
+  def extract_tar_extract(source)
+    ChefSpec::Matchers::ResourceMatcher.new(:tar_extract, :extract, source)
+  end
+
+  def extract_local_tar_extract(source)
+    ChefSpec::Matchers::ResourceMatcher.new(:tar_extract, :extract_local, source)
+  end
+end
