d0dfb77dd7819c61320e11ce7d69f8d0f55a4d05
Tim Smith <tsmith@chef.io>
2017-01-17 20:41:23 -0800

    Add platforms to the metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,11 @@ description      'Installs tar and two resources to manage remote tar packages'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.1.0'
 
+
+%w(ubuntu debian redhat centos suse opensuse opensuseleap scientific oracle amazon zlinux).each do |os|
+  supports os
+end
+
 source_url 'https://github.com/chef-cookbooks/tomcat'
 issues_url 'https://github.com/chef-cookbooks/tomcat/issues'
 chef_version '>= 12.1' if respond_to?(:chef_version)
