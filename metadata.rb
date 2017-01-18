fc051dfeaaec0730028ab0d50cbf368551b173de
Tim Smith <tsmith@chef.io>
2017-01-18 10:59:15 -0800

    Cookstyle fix
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,6 @@ description      'Installs tar and two resources to manage remote tar packages'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.1.0'
 
-
 %w(ubuntu debian redhat centos suse opensuse opensuseleap scientific oracle amazon zlinux).each do |os|
   supports os
 end
