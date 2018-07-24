5cba895f7a1d5f826360e11c2c395ebfeb2f9b9c
Tim Smith <tsmith@chef.io>
2018-07-24 14:41:02 -0700

    Release 2.2.0
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
 description      'Installs tar and two resources to manage remote tar packages'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.1.1'
+version '2.2.0'
 
 %w(ubuntu debian redhat centos suse opensuse opensuseleap scientific oracle amazon zlinux freebsd).each do |os|
   supports os
